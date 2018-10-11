2d16bb8be04b5c17baca526ec3f199d8335a51bd
suellencpsantos <suellencristinep.santos@hotmail.com>
2018-10-10 23:39:23 -0300

    test

---
+++ Projeto/JS/setup.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	
+	$('#pg').jphotogrid({
+		baseCSS: {
+			width: '20%',
+			height: '25%',
+			padding: '0px'
+		},
+		selectedCSS: {
+			top: '50px',
+			left: '100px',
+			width: '70%',
+			height: '70%',
+			padding: '10px',
+			paddingBottom: '30px'
+		}
+	});
+		
+});
---
+++ README.md
@@ -0,0 +1,1 @@
+# suellencpsantos.github.io
